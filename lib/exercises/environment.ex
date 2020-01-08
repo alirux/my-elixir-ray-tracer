084f941cd7e180c247111ab6a74569ba4311151b
Alberto Lirussi <a.lirussi@gmail.com>
2020-01-08 10:51:22 +0100

    Added some documentation

--- lib/exercises/environment.ex
+++ lib/exercises/environment.ex
@@ -1,5 +1,7 @@
 defmodule MyElixirRayTracer.Exercises.Environment do
-
+@moduledoc """
+Environment model for exercise
+"""
   defstruct gravity: nil, wind: nil
 
 end
--- lib/exercises/projectile.ex
+++ lib/exercises/projectile.ex
@@ -1,4 +1,7 @@
 defmodule MyElixirRayTracer.Exercises.Projectile do
+  @moduledoc """
+  Projectile model for the exercise
+  """
 
   defstruct position: nil, velocity: nil
 
