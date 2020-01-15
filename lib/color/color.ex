7d9097b37fd64802f3985035b849fda3c27807c6
Alberto Lirussi <alberto.lirussi@infocert.it>
2020-01-15 23:19:07 +0100

    Canvas and tests

--- lib/color/color.ex
+++ lib/color/color.ex
@@ -35,4 +35,39 @@ defmodule MyElixirRayTracer.Color do
   """
   def hadamard_prod(c1, c2), do: %Color { red: c1.red * c2.red, green: c1.green * c2.green, blue: c1.blue * c2.blue }
 
+  @doc """
+  Scale a decimal number to an integer number 0..255
+  ### Examples
+    iex> MyElixirRayTracer.Color.scale_color_component(-1)
+    0
+
+    iex> MyElixirRayTracer.Color.scale_color_component(2)
+    255
+
+    iex> MyElixirRayTracer.Color.scale_color_component(0)
+    0
+
+    iex> MyElixirRayTracer.Color.scale_color_component(1)
+    255
+
+    iex> MyElixirRayTracer.Color.scale_color_component(0.5)
+    128
+  """
+  def scale_color_component(p) do
+    d = min(p, 1.0)
+    d = max(d, 0)
+    round(255*d)
+  end
+
+  @doc """
+  Scale all the components of a color from 0..1 (float) to 0..255 (integer)
+  ### Examples
+    iex> MyElixirRayTracer.Color.color_to_decimal(MyElixirRayTracer.Color.color(0, 0.5, 1))
+    MyElixirRayTracer.Color.color(0, 128, 255)
+
+  """
+  def color_to_decimal(c) do
+    color(scale_color_component(c.red), scale_color_component(c.green), scale_color_component(c.blue))
+  end
+
 end
