ee92c1d1d9e026b925748db1a0ef8a3feec9d388
Alberto Lirussi <a.lirussi@gmail.com>
2020-01-31 12:03:08 +0100

    Added matrix inversion

--- test/matrix/matrix_test.exs
+++ test/matrix/matrix_test.exs
@@ -185,4 +185,44 @@ defmodule MyElixirRayTracerTestr.Matrix do
     assert matrix_determinant(m) == -4071
   end
 
+  test "Testing an invertible matrix for invertibility" do
+    m = matrix4x4(6, 4, 4, 4, 5, 5, 7, 6, 4, -9, 3, -7, 9, 1, 7, -6)
+    assert matrix_is_invertible?(m) == true
+  end
+
+  test "Testing a noninvertible matrix for invertibility" do
+    m = matrix4x4(-4, 2, -2, -3, 9, 6, 2, 6, 0, -5, 1, -5, 0, 0, 0, 0)
+    assert matrix_is_invertible?(m) == false
+  end
+
+  test "Calculating the inverse of a matrix" do
+    m = matrix4x4(-5, 2, 6, -8, 1, -5, 1, 8, 7, 7, -6, -7, 1, -3, 7, 4)
+    expected_inverse = matrix4x4(0.21805, 0.45113, 0.24060, -0.04511, -0.80827, -1.45677, -0.44361, 0.52068, -0.07895, -0.22368, -0.05263, 0.19737, -0.52256, -0.81391, -0.30075, 0.30639)
+    { :ok, inverse } = matrix_inverse(m)
+    assert matrix_equals(inverse, expected_inverse) == [ equal: true ]
+  end
+
+  test "Calculating the inverse of another matrix" do
+    m = matrix4x4(8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4)
+    expected_inverse = matrix4x4(-0.15385, -0.15385, -0.28205, -0.53846, -0.07692, 0.12308, 0.02564, 0.03077, 0.35897, 0.35897, 0.43590, 0.92308, -0.69231, -0.69231, -0.76923, -1.92308)
+    { :ok, inverse } = matrix_inverse(m)
+    assert matrix_equals(inverse, expected_inverse) == [ equal: true ]
+  end
+
+  test "Calculating the inverse of a third matrix" do
+    m = matrix4x4(9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2)
+    expected_inverse = matrix4x4(-0.04074, -0.07778, 0.14444, -0.22222, -0.07778, 0.03333, 0.36667, -0.33333, -0.02901, -0.14630, -0.10926, 0.12963, 0.17778, 0.06667, -0.26667, 0.33333)
+    { :ok, inverse } = matrix_inverse(m)
+    assert matrix_equals(inverse, expected_inverse) == [ equal: true ]
+  end
+
+  test "Multiplying a product by its inverse" do
+    m1 = matrix4x4(3, -9, 7, 3, 3, -8, 2, -9, -4, 4, 4, 1, -6, 5, -1, 1)
+    m2 = matrix4x4(8, 2, 2, 2, 3, -1, 7, 0, 7, 0, 5, 4, 6, -2, 0, 5)
+    { :ok, c } = matrix_multiply(m1, m2)
+    { :ok, m2_inverse } = matrix_inverse(m2)
+    { :ok, res } = matrix_multiply(c, m2_inverse)
+    assert matrix_equals(res, m1) == [ equal: true ]
+  end
+
 end
