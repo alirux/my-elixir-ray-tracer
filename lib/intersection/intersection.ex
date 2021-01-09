0204ce694369be7110134080bebeba026e0c8cc8
Alberto Lirussi <alberto.lirussi@infocert.it>
2021-01-09 16:36:13 +0100

    ADD intersections documentation

--- lib/intersection/intersection.ex
+++ lib/intersection/intersection.ex
@@ -6,15 +6,24 @@ defmodule MyElixirRayTracer.Intersection do
     %MyElixirRayTracer.Intersection {time: time, object: object}
   end
 
-  def intersections(old_intersections, new_intersection) when is_list(old_intersections) and is_struct(new_intersection, MyElixirRayTracer.Intersection) do
+  @doc """
+  Add a new intersection to a list of intersections maintaining the time order (asc)
+  """
+  def intersections_add(old_intersections, new_intersection) when is_list(old_intersections) and is_struct(new_intersection, MyElixirRayTracer.Intersection) do
     [new_intersection | old_intersections] |> Enum.sort(&(&1.time < &2.time))
   end
 
+  @doc """
+  Build a list of two intersections ordered by the time (asc)
+  """
   def intersections(i1, i2) when is_struct(i1, MyElixirRayTracer.Intersection) and is_struct(i2, MyElixirRayTracer.Intersection) do
     [i1 | [i2 | []]] |> Enum.sort(&(&1.time < &2.time))
     #intersections([], i1) |> intersections(i2)
   end
 
+  @doc """
+  Find the first non negative intersection: a hit between the ray and an object
+  """
   def hit(intersections) do
     #List.first(intersections)
     Enum.find(intersections, &(&1.time >= 0))
