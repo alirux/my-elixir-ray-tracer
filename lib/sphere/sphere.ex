63dcb58d2cad14a574e583786ef79aa4d44f9ad2
Alberto Lirussi <alberto.lirussi@infocert.it>
2021-01-16 18:42:12 +0100

    ADD more sphere_normal_at tests

--- lib/sphere/sphere.ex
+++ lib/sphere/sphere.ex
@@ -17,9 +17,14 @@ defmodule MyElixirRayTracer.Sphere do
   Finf the normal on a sphere at the point specified
   """
   def sphere_normal_at(sphere, world_point) do
+    #IO.inspect(world_point)
     object_point = sphere.transform |> matrix_inverse!() |> tuple_transform(world_point)
+    #IO.inspect(object_point)
     object_normal = minus(object_point, point(0, 0, 0))
+    #IO.inspect(object_normal)
     world_normal = sphere.transform |> matrix_inverse!() |> matrix_transpose() |> tuple_transform(object_normal)
+    #IO.inspect(world_normal)
+    #IO.inspect(magnitude(world_normal))
     normalize %MyElixirRayTracer.Tuple{ world_normal | w: 0 }
   end
 
--- test/sphere/sphere_test.exs
+++ test/sphere/sphere_test.exs
@@ -55,11 +55,91 @@ defmodule MyElixirRayTracerTest.Sphere do
     assert tuple_equal?(n, vector(0, 0.70711, -0.70711))
   end
 
-  test "Computing the normal on a transformed sphere" do
+  test "Computing the normal on a sqrt(3)/3" do
+    s = sphere(identity_matrix4x4() |> rotation_y(:math.pi/4))
+    n = sphere_normal_at(s, point(:math.sqrt(3)/3, :math.sqrt(3)/3, :math.sqrt(3)/3))
+    assert tuple_equal?(n, vector(:math.sqrt(3)/3, :math.sqrt(3)/3, :math.sqrt(3)/3))
+  end
+
+  test "Computing the normal on a +1y" do
+    s = sphere(identity_matrix4x4() |> translation(0, 1, 0))
+    n = sphere_normal_at(s, point(0, 2, 0))
+    assert tuple_equal?(n, vector(0, 1, 0))
+  end
+
+  @doc """
+     ˆ y
+     |
+  ---+O--> x
+     |
+     |
+  """
+  test "Computing the normal on a +1y +pi/2x" do
+    # The rotation is left-handed -> in the end the sphere is on the right of the y axis
+    s = sphere(identity_matrix4x4() |> translation(0, 1, 0) |> rotation_x(:math.pi/2))
+    n = sphere_normal_at(s, point(0, 0, 2))  # z >= 2
+    assert tuple_equal?(n, vector(0, 0, 1))
+    n = sphere_normal_at(s, point(0, 0, -1))  # z <= 0
+    assert tuple_equal?(n, vector(0, 0, -1))
+
+  end
+
+  test "Computing the normal on a -1z" do
+    s = sphere(identity_matrix4x4() |> translation(0, 0, -1))
+    n = sphere_normal_at(s, point(0, 0, -2))
+    assert tuple_equal?(n, vector(0, 0, -1))
+  end
+
+  test "Computing the normal on a scaled sphere" do
     s = sphere(identity_matrix4x4() |> scaling(1, 0.5, 1))
     n = sphere_normal_at(s, point(0, :math.sqrt(2)/2, -:math.sqrt(2)/2))
     assert tuple_equal?(n, vector(0, 0.97014, -0.24254))
   end
+
+  test "Computing the normal on a scaled sphere 2" do
+    s = sphere(identity_matrix4x4() |> scaling(1, 0.5, 1))
+    n = sphere_normal_at(s, point(0, 0.5, 0))
+    assert tuple_equal?(n, vector(0, 1, 0))
+  end
+
+  test "Computing the normal on a scaled sphere 3" do
+    s = sphere identity_matrix4x4() |> scaling(1, 0.5, 1) |> rotation_z(:math.pi/2)
+    n = sphere_normal_at s, point(0, 0, 20373) # doesn't care the z value, can be 0.5, 1, ...
+    assert tuple_equal? n, vector(0, 0, 1)
+  end
+
+  test "Computing the normal on a traslated sphere 4" do
+    s = sphere identity_matrix4x4() |> translation(0, 1, 0)
+    n = sphere_normal_at s, point(1, 1, 0)
+    assert tuple_equal? n, vector(1, 0, 0)
+  end
+
+  @doc """
+     ˆ y
+     |
+  --O+---> x
+     |
+     |
+  """
+  test "Computing the normal on a traslated sphere 5" do
+    # The rotation is left-handed -> in the end the sphere is on the left of the y axis
+    s = sphere identity_matrix4x4() |> translation(0, 1, 0) |> rotation_z(:math.pi/2)
+    n = sphere_normal_at s, point(-0.5, 0.5, 0)
+    assert tuple_equal? n, vector(:math.sqrt(2)/2, :math.sqrt(2)/2, 0)
+  end
+
+  test "Computing the normal on a traslated sphere 6" do
+    s = sphere identity_matrix4x4() |> translation(0, 1, 0)
+    n = sphere_normal_at s, point(0.5, 1.5, 0)
+    assert tuple_equal? n, vector(:math.sqrt(2)/2, :math.sqrt(2)/2, 0)
+  end
+
+  test "Computing the normal on a transformed sphere" do
+    s = sphere identity_matrix4x4() |> translation(0, 1, 0) |> scaling(1, 0.5, 1)
+    n = sphere_normal_at s, point(1, 0.5, 0)
+    assert tuple_equal? n, vector(1, 0, 0)
+  end
+
 
 
 end
