5673363406da30016348d86872c80231099fb634
Alberto Lirussi <a.lirussi@gmail.com>
2026-05-03 20:12:46 +0200

    Add Gigalixir release deployment configuration
    - Add .tool-versions with Elixir 1.19.5-otp-28 and Erlang 28.2
    - Configure mix releases in root mix.exs for the ray_tracer_web release
    - Add Procfile to start the release on Gigalixir

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: _build/prod/rel/ray_tracer_web/bin/ray_tracer_web start
--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule MyElixirRayTracer.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      listeners: [Phoenix.CodeReloader]
+      listeners: [Phoenix.CodeReloader],
+      releases: [
+        ray_tracer_web: [
+          applications: [
+            my_elixir_ray_tracer: :permanent,
+            ray_tracer_web: :permanent
+          ]
+        ]
+      ]
     ]
   end
 
